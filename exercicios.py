030dbefd66545d62f7324a9c3809c3a0012680b2
Gilson <gilsoncruz10@gmail.com>
2025-01-24 20:58:19 -0300

    adicionado exercicio 11

--- exercicios.py
+++ exercicios.py
@@ -67,10 +67,11 @@ print(f"O número {numero} elevado a potência {potencia} dá {resultado}.")
 '''
 
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
-
+'''
 celsius = float(input("insira a temperatura em graus celsius (somente números): "))
 fahr = (celsius * 9 / 5) + 32
 print(f"A temperatura em fahrenheit é {fahr} oF")
+'''
 
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
 
@@ -82,6 +83,12 @@ print(f"A temperatura em fahrenheit é {fahr} oF")
 # #### Strings (`str`)
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+
+nome = input("escreva seu nome: ")
+caps = nome.upper()
+print(f"seu nome em maiúsculas é {caps}.")
+#print(f"seu nome em maiúsculas é {nome.upper()}.")
+
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
