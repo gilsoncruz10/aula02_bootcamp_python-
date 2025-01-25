af17cbc0b0c3d2407fec797f18aee84ede1c43a9
Gilson <gilsoncruz10@gmail.com>
2025-01-24 21:25:56 -0300

    adicionado exercicio 12

--- exercicios.py
+++ exercicios.py
@@ -83,13 +83,20 @@ print(f"A temperatura em fahrenheit é {fahr} oF")
 # #### Strings (`str`)
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
-
+'''
 nome = input("escreva seu nome: ")
 caps = nome.upper()
 print(f"seu nome em maiúsculas é {caps}.")
 #print(f"seu nome em maiúsculas é {nome.upper()}.")
+'''
 
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+
+nome = input("insira seu nome completo: ")
+#minusc = nome.lower()
+#print(f"seu nome em minúsculas é {minusc}")
+print(f"seu nome em minúsculas é {nome.lower()}")
+
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
